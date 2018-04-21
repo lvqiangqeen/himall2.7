910af07f8b83fab450ac50812f51e425a7188af0
byue <yue19890924@163.com>
2018-04-21 10:14:15 +0800

    taocan commit

---
+++ himall2.7x/Himall.Web/Areas/Web/Controllers/TaoCanController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Himall.Web.Areas.Web.Controllers
+{
+	public class TaoCanController : Controller
+	{
+		// GET: Web/TaoCan
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult TaoCanInfo(int id)
+		{
+			return View();
+		}
+	}
+}
